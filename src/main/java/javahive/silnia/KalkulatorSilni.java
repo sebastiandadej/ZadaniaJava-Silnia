c59039723b25158a878fc57187ad84ad34dc2b3d
Marcin <mjg@g.pl>
2013-10-14 17:16:18 +0200

    test gita, sprawdzam czy zmiana trafi na serwer

--- src/main/java/javahive/silnia/KalkulatorSilni.java
+++ src/main/java/javahive/silnia/KalkulatorSilni.java
@@ -4,9 +4,10 @@
  */
 package javahive.silnia;
 /**
- *
+ * uwaga - test gita
  * @author Marcin Grabowiecki
  */
+
 public interface KalkulatorSilni {
     public String licz(int arg) throws ArithmeticException;
 }
